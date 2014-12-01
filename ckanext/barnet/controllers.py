55c7328a26190332c09221a892914c92e92311ec
joetsoi <joe.yeung.tsoi@gmail.com>
2014-12-01 05:10:32 +0000

    add auth check to export csv controller

--- ckanext/barnet/controllers.py
+++ ckanext/barnet/controllers.py
@@ -2,6 +2,7 @@ import json
 import os.path
 import inspect
 
+import ckan.model as model
 import ckan.plugins.toolkit as toolkit
 import ckanapi_exporter.exporter as exporter
 
@@ -20,6 +21,15 @@ def this_directory():
 class CSVExportController(toolkit.BaseController):
 
     def export(self):
+
+        context = {'model': model, 'user': toolkit.c.user,
+                   'auth_user_obj': toolkit.c.userobj}
+        try:
+            toolkit.check_access('export_csv', context, {})
+        except toolkit.NotAuthorized:
+            toolkit.abort(401, toolkit._(
+                'Need to be organization administrator to export as CSV')
+            )
         columns = os.path.join(this_directory(), "columns.json")
         csv_string = exporter.export("https://open.barnet.gov.uk/",
                                      columns=columns)
